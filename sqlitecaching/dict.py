fe74e2dfd42d87cb6265dbb1d18e39a1ad6a1978
Tom Saunders <tom.saunders@paradoxic.al>
2020-08-28 02:08:38 +0100

    Restrict api ?

--- sqlitecaching/dict.py
+++ sqlitecaching/dict.py
@@ -3,7 +3,15 @@ from collections import UserDict
 
 
 class CacheDict(UserDict):
-    def __init__(self, *, placeholder=None):
+    __please_do_not_construct_directly = object()
+
+    def __init__(self, *, flag=None, placeholder=None):
         super().__init__()
+        if flag != self.__please_do_not_construct_directly:
+            raise Exception("e")
         self.placeholder = placeholder
         self.db = sqlite3.connect(":memory:")
+
+    @classmethod
+    def create_from_connection(cls):
+        return CacheDict(flag=cls.__please_do_not_construct_directly,)
--- test/test_create_cachedict.py
+++ test/test_create_cachedict.py
@@ -5,5 +5,5 @@ from sqlitecaching.dict import CacheDict
 
 class TestCreateCacheDict(unittest.TestCase):
     def test_create_noargs(self):
-        c = CacheDict()
+        c = CacheDict.create_from_connection()
         self.assertNotEqual(c, None)
