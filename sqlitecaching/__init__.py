862b8a80e74d540336d70f779cfb8164224b6a6e
Tom Saunders <tom.saunders@paradoxic.al>
2020-08-29 12:09:49 +0100

    Logging handling in the main module.
    Probably this Config should be commonly defined elsewhere and then
    subclassed/instantiated where it's needed rather than writing this
    over and over again.

--- sqlitecaching/__init__.py
+++ sqlitecaching/__init__.py
@@ -1,6 +1,19 @@
 class Config:
+    __log_parent = None
     __log_base = "sqlitecaching"
 
     @classmethod
+    def setup_logging(cls):
+        pass
+
+    @classmethod
     def get_log_base(cls):
-        return cls.__log_base
+        if cls.__log_parent:
+            return f"{cls.__log_parent}.{cls.__log_base}"
+        else:
+            return cls.__log_base
+
+    @classmethod
+    def set_log_parent(cls, log_parent):
+        cls.__log_parent = log_parent
+        cls.setup_logging()
